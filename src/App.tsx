7a101a32dec20f7bf3655de17591bab099f7687a
csandoval18 <christian_alvites@outlook.com>
2022-11-02 18:18:26 -0500

    implemented navbar

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,13 @@ import { useState } from 'react'
 import Navbar from "./components/Navbar"
 
 function App() {
-  const [theme, setTheme] = useState('light')
+  const [theme, setTheme] = useState('dark')
   return (
     <div className={"App " + theme}>
       <div className="background flex">
-        <button onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}>change theme</button>
         <Navbar />
 
+        <button onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}>change theme</button>
       </div>
     </div>
   )
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,19 @@
 import React from 'react'
+import '../style/Navbar.scss'
 
 interface NavbarProps { }
 
 const Navbar: React.FC<NavbarProps> = () => {
   return (
-    <nav></nav>
+    <nav>
+      <div>CAS</div>
+      <ul>
+        <li><a href="#">About</a></li>
+        <li><a href="#"></a>Resume</li>
+        <li><a href="#">Portfolio</a></li>
+        <li><a href="#">Contact</a></li>
+      </ul>
+    </nav>
   )
 }
 
